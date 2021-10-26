195fa95c5c2c3935f7dff8f5f796e7af30a823ec
100licki <57677238+100licki@users.noreply.github.com>
2021-10-26 13:16:12 +0200

    Delete Online-shop-spring-basket directory

--- Online-shop-spring-basket/src/main/java/com/onlineshop/productcatalog/ProductStorage.java
+++
@@ -1,6 +0,0 @@
-package com.onlineshop.productcatalog;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductStorage extends JpaRepository<Product, String> {
-}
--- Online-shop-spring-basket/src/main/java/com/onlineshop/productcatalog/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.onlineshop.productcatalog.exceptions;
-
-public class ProductNotFoundException extends IllegalStateException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
